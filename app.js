73d131b25a1d096ca743a8693e37b64db18b3fe4
sugarshin <shinsugar@gmail.com>
2015-04-11 12:15:28 +0900

    edit

--- app.js
+++ app.js
@@ -1,30 +1,38 @@
+/*!
+ * @license display-fname
+ * (c) sugarshin
+ * License: MIT
+ */
+
 var fs = require('fs');
 var glob = require('glob');
 var argv = process.argv;
 
-function showConsole(data) {
+function stdout(data) {
   console.log(data);
 }
 
 function writeFile(data, filePath) {
   fs.writeFile(filePath, data, function (err) {
     if (err) return console.log('File error: ' + err);
-    console.log('Success!!');
+    console.log('Success!! => ' + filePath);
   });
 }
 
-glob(argv[2] + '/**/*', function(err, files) {
-  var d = '';
-  files.forEach(function(el, i, arr) {
-    var stat = fs.statSync(el);
-    if (!stat.isDirectory()) {
-      d += el + '\n';
+function displayFname(path, writeFileName) {
+  glob(path + '/**/*', function(err, files) {
+    var data = files.map(function(el, i) {
+      if (!fs.statSync(el).isDirectory()) return el;
+    }).filter(function(el, i) {
+      if (el) return el;
+    }).join('\n');
+
+    if (writeFileName) {
+      writeFile(data, writeFileName);
+    } else {
+      stdout(data);
     }
   });
+}
 
-  if (argv[3]) {
-    writeFile(d, argv[3]);
-  } else {
-    showConsole(d);
-  }
-});
+displayFname(argv[2], argv[3]);
--- readme.md
+++ readme.md
@@ -2,18 +2,18 @@
 
 Display file name
 
-```
+```zsh
 node app.js 'directory/path/to'
 
-// directory/path/to/file1.txt
-// directory/path/to/file2.txt
-// directory/path/to/dir/file.txt
-// directory/path/to/dir/dir2/file.txt
+# directory/path/to/file1.txt
+# directory/path/to/file2.txt
+# directory/path/to/dir/file.txt
+# directory/path/to/dir/dir2/file.txt
 ```
 
 write file
 
-```
+```zsh
 node app.js 'directory/path/to' filename.txt
 ```
 
